cbaaa53a263bb9152ad1ca13841bb1cfb47b1288
Neerajan <nirajanbohora@gmail.com>
2026-07-20 10:12:43 +0545

    update

--- Script/details.js
+++ Script/details.js
@@ -1,22 +1,57 @@
-    $(document).ready(function () {
-            $("#from").datepicker({
-                dateFormat: "dd M yy",
-                minDate: 0,
-                onSelect: function (selectedDate) {
-                    $("#to").datepicker("option", "minDate", selectedDate);
-                }
-            });
+$(document).ready(function () {
 
-            $("#to").datepicker({
-                dateFormat: "dd M yy",
-                minDate: 0
-            });
+    $("#from").datepicker({
+        dateFormat: "dd M yy",
+        minDate: 0,
+        onSelect: function (selectedDate) {
+            $("#to").datepicker("option", "minDate", selectedDate);
+        }
+    });
 
-            $("#guest").select2({
-                minimumResultsForSearch: Infinity,
-                width: "100%"
-            });
+    $("#to").datepicker({
+        dateFormat: "dd M yy",
+        minDate: 0
+    });
+
+    $("#guest").select2({
+        minimumResultsForSearch: Infinity,
+        width: "100%"
+    });
+
+    // ==========================
+    // Related Tours Slider
+    // ==========================
+
+    const grid = document.getElementById("relatedGrid");
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+
+    const scrollAmount = 322; 
+
+    nextBtn.addEventListener("click", function () {
+        grid.scrollBy({
+            left: scrollAmount,
+            behavior: "smooth"
         });
+    });
 
-        fetch("nav.html").then(res => res.text()).then(data => { document.getElementById("navbar").innerHTML = data; });
-        fetch("footer.html").then(res => res.text()).then(data => { document.getElementById("footer").innerHTML = data; });
+    prevBtn.addEventListener("click", function () {
+        grid.scrollBy({
+            left: -scrollAmount,
+            behavior: "smooth"
+        });
+    });
+
+});
+
+fetch("nav.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    });
+
+fetch("footer.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+    });
